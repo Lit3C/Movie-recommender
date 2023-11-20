3a349bb32ee15f267050296330fa40acb6fe18ad
LiNa <lina.pich@gmail.com>
2023-11-20 23:22:43 +0100

    create area_chart page

--- user_app/urls.py
+++ user_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('area-chart', views.area_chart, name='app-area-chart'),
     path('', views.index, name='app-index'),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def area_chart(request):
+    return render(request, 'area_chart.html')
