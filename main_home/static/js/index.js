4164a76df5a1dac7c69bb8202ec173a74920b642
LiNa <lina.pich@gmail.com>
2023-11-21 10:38:00 +0100

    first chart from Js on dashboard

--- main_home/static/js/index.js
+++ main_home/static/js/index.js
@@ -1,1 +1,22 @@
-console.log("YES")
+const totalViewChart = document.getElementById('total-views-chart');
+const revenueChart = document.getElementById('revenue-chart');
+const growthRateChart = document.getElementById('growth-rate-chart');
+
+new Chart(totalViewChart, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+        datasets: [{
+            labels: '# of votes',
+            data: [12545, 19512, 37897, 54574, 29564, 44547],
+            borderWidth: 1,
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
