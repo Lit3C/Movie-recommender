ff8f5b12ed75f9165d014dc6f5eed889b68777ad
LiNa <lina.pich@gmail.com>
2023-11-21 08:45:23 +0100

    create dashboard page

--- main_home/urls.py
+++ main_home/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard/', include("dashboard.urls")),
     path('', include("user_app.urls")),
 ]
