fb512a5e758f509b18f5e3d781004f4ff85236c0
LiNa <lina.pich@gmail.com>
2023-11-21 12:29:16 +0100

    init Js files for movies,casting,market,userstats

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'dashboard.html')
+
+def movies(request):
+    return render(request, 'movies.html')
+
+def casting(request):
+    return render(request, 'casting.html')
+
+def market(request):
+    return render(request, 'market.html')
+
+def userstats(request):
+    return render(request, 'userstats.html')
