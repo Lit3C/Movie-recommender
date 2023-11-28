7aafcb69e268306bc21685de2610bb6ddee43b4f
LiNa <lina.pich@gmail.com>
2023-11-28 16:17:52 +0100

    ML v13 OK !

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
+from . import models
 
 urlpatterns = [
     path('', views.index, name='app-index'),
-    # path('datatables', views.datatables, name='app-datatables'),
-    # path('area-chart', views.area_chart, name='app-area-chart'),
+    path('result', views.reco_sys, name='app-result'),
     path('search', views.search, name='app-search'),
     path('list', views.list, name='app-list'),
     path('booking', views.booking, name='app-booking'),
--- user_app/views.py
+++ user_app/views.py
@@ -1,11 +1,97 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from sklearn.neighbors import NearestNeighbors
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+import json
+import os
 
-# Create your views here.
+df_table_neighbor = pd.read_pickle('user_app/data/table_finale_dummies.pickle')
+
+from sklearn.preprocessing import StandardScaler
+
+# def reco_sys(request):
+#     print("ok",request)
+#     if request.method == 'POST':
+#         film_name = request.POST['film-search']
+#         # film_name = request.POST.get('film-search')
+#         # film_name = "Avatar"  
+#         print(film_name)
+        
+#         film_name_contains = df_table_neighbor['originalTitle'].str.contains(film_name, case= False)
+
+#         X = df_table_neighbor.drop(columns = ['tconst', 'primaryTitle', 'director_x', 'vote_count', 'vote_average', 'averageRating', 'originalTitle', 'genres_x','runtime', 'overview', 'cast',
+#         'director_y', 'original_language', 'poster_path', 'startYear'])
+
+#         film_features = df_table_neighbor.loc[film_name_contains, X.columns]
+
+#         scaler = StandardScaler()
+#         X_scaled = scaler.fit_transform(X)
+#         film_features_scaled = scaler.transform(film_features)
+
+#         model_films = NearestNeighbors(n_neighbors= 9)
+#         model_films.fit(X_scaled)
+
+#         neighbors = model_films.kneighbors(film_features_scaled)
+
+#         closest_films_index = neighbors[1][0]
+#         closest_films = df_table_neighbor[['originalTitle', 'averageRating', 'vote_count', 'startYear', 'genres_x', 'runtime', 'overview', 'cast', 'director_y', 'original_language', 'poster_path']].iloc[closest_films_index]
+#         distances = neighbors[0][0]
+
+#         context = {
+#             'film_name': film_name, 
+#             'closest_films': closest_films,
+#             'distances': distances
+#         }
+
+#         return render(request, 'app/result.html', context)
+
 def index(request):
     return render(request, 'app/app.html')
 
 def search(request):
-    return render(request, 'app/search.html')
+    context = {'a':'Hello World'}
+    return render(request, 'app/search.html', context)
+
+# def result(request):
+#     return render(request, 'app/result.html')
+
+def reco_sys(request):
+    print(request)
+    if request.method == 'POST':
+        film_name = request.POST['film-search']
+        print("HellooooWorld")
+        print(film_name)
+        film_name_contains = df_table_neighbor['originalTitle'].str.contains(film_name, case= False)
+
+        X = df_table_neighbor.drop(columns = ['tconst', 'primaryTitle', 'director_x', 'vote_count', 'vote_average', 'averageRating', 'originalTitle', 'genres_x','runtime', 'overview', 'cast',
+        'director_y', 'original_language', 'poster_path', 'startYear'])
+
+        film_features = df_table_neighbor.loc[film_name_contains, X.columns]
+
+        scaler = StandardScaler()
+        X_scaled = scaler.fit_transform(X)
+        film_features_scaled = scaler.transform(film_features)
+
+        model_films = NearestNeighbors(n_neighbors= 9)
+        model_films.fit(X_scaled)
+
+        neighbors = model_films.kneighbors(film_features_scaled)
+
+        closest_films_index = neighbors[1][0]
+        closest_films = df_table_neighbor[['originalTitle', 'averageRating', 'vote_count', 'startYear', 'genres_x', 'runtime', 'overview', 'cast', 'director_y', 'original_language', 'poster_path']].iloc[closest_films_index]
+        distances = neighbors[0][0]
+
+        context = {
+            'film_name': film_name, 
+            'closest_films': closest_films,
+            'distances': distances
+        }
+    return render(request, 'app/search.html', context)
+    
+    
 
 def list(request):
     return render(request, 'app/list.html')
