7fc548528c4931d86dc4597c387848ad0f4470ca
LiNa <lina.pich@gmail.com>
2023-11-30 10:50:13 +0100

    last knn function update

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
 from . import models
+from .views import movie_list
 
 urlpatterns = [
     path('', views.index, name='app-index'),
     path('result', views.reco_sys, name='app-result'),
     path('result', views.reco_sys_click, name='app-result-click'),
-    path('search', views.search, name='app-search'),
+    path('search', movie_list, name='app-search'),
     path('list', views.lst, name='app-list'),
     path('booking', views.booking, name='app-booking'),
     path('settings', views.settings, name='app-settings'),
